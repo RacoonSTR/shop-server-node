3f775b8d72f040fcb48806e9a7f1a7670bfaf02e
AlexeyKiselev <racoonstr@gmail.com>
2020-09-03 00:45:43 +0300

    Mongoose Client

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
 import express = require('express');
+import db = require('./db/db');
 const app: express.Application = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
   console.log('App is listening on port 3000!');
+  await start()
 });
+
+async function start() {
+  await db.connect();
+}
