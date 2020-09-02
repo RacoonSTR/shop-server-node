59b847b2549593eb1f7a8603242a7af4ac33f423
AlexeyKiselev <racoonstr@gmail.com>
2020-09-02 22:18:36 +0300

    Add new models

---
+++ schemas/category.ts
@@ -0,0 +1,9 @@
+import mongoose = require('mongoose');
+
+const Category = new mongoose.Schema({
+    categoryId: mongoose.Schema.Types.ObjectId,
+    title: String,
+    specs: [String] //TODO: Add specs
+});
+
+module.exports = Category;
